be10c2463940314cce4f67911ef4050955412a68
Luiz Felipe <luiz.lipe30@gmail.com>
2021-11-30 11:04:18 -0300

    App Tempo

---
+++ README.md
@@ -0,0 +1,5 @@
+# Previsão do tempo em Flutter
+
+Aplicativo híbrido que mostra a previsão do tempo.
+Pode usar a localização do GPS ou o nome da cidade informada pelo usuário.
+O aplicativo coleta as informações do OpenWeather API.
---
+++ lib/widgets/forecasts.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'forecast_box_widget.dart';
+
+class Forecasts extends StatefulWidget {
+  var data;
+
+  Forecasts({this.data});
+
+  @override
+  _ForecastsState createState() => _ForecastsState();
+}
+
+class _ForecastsState extends State<Forecasts> {
+  @override
+  Widget build(BuildContext context) {
+    print(widget.data);
+
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      children: <Widget>[
+        /*
+        ForecastBox(
+          displayDay: "Amanhã",
+          medianTemperature: 30,
+          minTemperature: 17,
+          maxtemperature: 30,
+          weatherIcon: "☀",
+        ),
+        ForecastBox(
+          displayDay: "Dia 21",
+          medianTemperature: 22,
+          minTemperature: 16,
+          maxtemperature: 24,
+          weatherIcon: "☁️",
+        ),
+        ForecastBox(
+          displayDay: "Dia 22",
+          medianTemperature: 22,
+          minTemperature: 16,
+          maxtemperature: 24,
+          weatherIcon: "☁️",
+        ),
+        */
+      ],
+    );
+  }
+}
